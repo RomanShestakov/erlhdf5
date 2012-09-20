fdbce277f67e6b40e24ef063a59ae733a6914ef5
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-09-20 21:00:34 +0100

    small test refactoring

--- test/erlh5f_SUITE.erl
+++ test/erlh5f_SUITE.erl
@@ -9,36 +9,15 @@ suite() ->
     [{timetrap,{seconds, 60}}].
 
 init_per_suite(Config) ->
-    %% create / close data file
-    {ok, File} = erlhdf5:h5fcreate("hdf5.h5", 'H5F_ACC_TRUNC'),
-    %% 2d array with 3 columns
-    {ok, Space} = erlhdf5:h5screate_simple(2, {100, 3}),
-    {ok, Dcpl} = erlhdf5:h5pcreate('H5P_DATASET_CREATE'),
-    {ok, Type} = erlhdf5:h5tcopy('H5T_NATIVE_INT'),
-    %% create new dataset
-    {ok, DS} = erlhdf5:h5dcreate(File, "/dset", Type, Space, Dcpl),
+    Config.
 
-    %% close resources
-    ok = erlhdf5:h5sclose(Space),
-    ok = erlhdf5:h5pclose(Dcpl),
-    ok = erlhdf5:h5tclose(Type),
-    ok = erlhdf5:h5dclose(DS),
-    [{file, File} | Config].
-
-end_per_suite(Config) ->
-    File = ?config(file, Config),
-    ok = erlhdf5:h5fclose(File),
+end_per_suite(_Config) ->
     ok.
 
 init_per_testcase(_TestCase, Config) ->
-    File = ?config(file, Config),
-    %% open existing dataset
-    {ok, DS} = erlhdf5:h5dopen(File, "/dset"),
-    [{file, File}, {dataset, DS} | Config].
+    Config.
 
-end_per_testcase(_TestCase, Config) ->
-    DS = ?config(dataset, Config),
-    ok = erlhdf5:h5dclose(DS),
+end_per_testcase(_TestCase, _Config) ->
     ok.
 
 all() ->
@@ -55,8 +34,18 @@ all() ->
 %% h5_write.c
 %% @end
 %%--------------------------------------------------------------------
-h5_write(Config) ->
-    DS = ?config(dataset, Config),
+h5_write(_Config) ->
+    %% create data file
+    {ok, File} = erlhdf5:h5fcreate("hdf5.h5", 'H5F_ACC_TRUNC'),
+
+    %% 2d array with 3 columns
+    {ok, Space} = erlhdf5:h5screate_simple(2, {100, 3}),
+    {ok, Dcpl} = erlhdf5:h5pcreate('H5P_DATASET_CREATE'),
+    {ok, Type} = erlhdf5:h5tcopy('H5T_NATIVE_INT'),
+
+    %% create new dataset
+    {ok, DS} = erlhdf5:h5dcreate(File, "/dset", Type, Space, Dcpl),
+
     {ok, Status} = erlhdf5:h5d_get_space_status(DS),
     {ok, Size} = erlhdf5:h5d_get_storage_size(DS),
     ct:log("dataset status: ~p, size: ~p ", [Status, Size]),
@@ -68,6 +57,12 @@ h5_write(Config) ->
     {ok, Status1} = erlhdf5:h5d_get_space_status(DS),
     {ok, Size1} = erlhdf5:h5d_get_storage_size(DS),
     ct:log("dataset status after write: ~p, size: ~p ", [Status1, Size1]),
+
+    %% close resources
+    ok = erlhdf5:h5sclose(Space),
+    ok = erlhdf5:h5pclose(Dcpl),
+    ok = erlhdf5:h5tclose(Type),
+    ok = erlhdf5:h5dclose(DS),
     ok.
 
 %%--------------------------------------------------------------------
@@ -105,45 +100,15 @@ h5_lite_write_read(_Config) ->
     ok.
 
 
-%% %%--------------------------------------------------------------------
-%% %% @doc
-%% %% h5_write.c
-%% %% @end
-%% %%--------------------------------------------------------------------
-%% h5_lite_read(_Config) ->
-%%     %% set some params
-%%     FileName = "hdf5_lt.h5",
-%%     %% Rank = 2,
-%%     DS_Name = "/dset_lt",
-
-%%     %% open new file
-%%     {ok, File} = erlhdf5:h5fopen(FileName, 'H5F_ACC_RDONLY'),
-
-%%     %% create dataset with rank = 2
-%%     {ok, Data} = erlhdf5:h5lt_read_dataset_int(File, DS_Name),
-
-%%     %% %% get dataset rank - should match to rank used in dataset creation
-%%     %% {ok, Rank} = erlhdf5:h5ltget_dataset_ndims(File, DS_Name),
-%%     %% ct:log("dataset rank : ~p ", [Rank]),
-
-%%     %% %% check ds info
-%%     %% {ok, DS_Info} = erlhdf5:h5ltget_dataset_info(File, DS_Name, Rank),
-%%     ct:log("dataset info : ~p ", [Data]),
-
-%%     %% {ok, Status1} = erlhdf5:h5d_get_space_status(DS),
-%%     %% {ok, Size1} = erlhdf5:h5d_get_storage_size(DS),
-%%     %% ct:log("dataset status : ~p ", [C]),
-%%     ok = erlhdf5:h5fclose(File),
-%%     ok.
-
-
 %%--------------------------------------------------------------------
 %% @doc
 %% h5_read.c
 %% @end
 %%--------------------------------------------------------------------
-h5_read(Config) ->
-    DS = ?config(dataset, Config),
+h5_read(_Config) ->
+    %% DS = ?config(dataset, Config),
+    {ok, File} = erlhdf5:h5fopen("hdf5.h5", 'H5F_ACC_RDONLY'),
+    {ok, DS} = erlhdf5:h5dopen(File, "/dset"),
 
     {ok, Type} = erlhdf5:h5dget_type(DS),
     {ok, Class} = erlhdf5:h5tget_class(Type),
@@ -168,33 +133,6 @@ h5_read(Config) ->
     %% close resources
     ok = erlhdf5:h5tclose(Type),
     ok = erlhdf5:h5sclose(Dataspace),
-
+    ok = erlhdf5:h5dclose(DS),
+    ok = erlhdf5:h5fclose(File),
     ok.
-
-
-%% write_example(_Config) ->
-%%     %% File = ?config(file, Config),
-%%     %% Space = ?config(space, Config),
-%%     %% Dcpl = ?config(dcpl, Config),
-%%     %% Type = ?config(type, Config),
-
-%%     %% % create dataset
-%%     %% {ok, DS} = erlhdf5:h5dcreate(File, "/dset", Type, Space, Dcpl),
-
-%%     %% {ok, Status} = erlhdf5:h5d_get_space_status(DS),
-%%     %% {ok, Size} = erlhdf5:h5d_get_storage_size(DS),
-%%     %% ct:log("dataset status: ~p, size: ~p ", [Status, Size]),
-
-%%     %write some data into dataset
-%%     {ok, File} = erlhdf5:h5fcreate("SDS1.h5", 'H5F_ACC_TRUNC'),
-%%     {ok, Space} = erlhdf5:h5screate_simple(2, {3, 5}),
-
-%%     ok = erlhdf5:h5dwrite_example(File, Space),
-
-%%     %% {ok, Status1} = erlhdf5:h5d_get_space_status(DS),
-%%     %% ct:log("dataset status after write, ~p ", [Status1]),
-
-%%     % close dataset
-%%     %ok = erlhdf5:h5dclose(DS),
-%%     ok.
-
